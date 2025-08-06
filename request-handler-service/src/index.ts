79da8940bd9af64612d7fe3bde59c9271af4723c
zunxii <zunxii.2210@gmail.com>
2025-08-06 23:20:03 +0530

    request service debug

--- request-handler-service/src/index.ts
+++ request-handler-service/src/index.ts
@@ -16,19 +16,17 @@ app.get("*", async (req, res) => {
     const host = req.hostname;
 
     const id = host.split(".")[0];
+    const filePath = req.path;
 
-    console.log(id);
-    // const filePath = req.path;
-
-    // const contents = await s3.getObject({
-    //     Bucket: "vercel",
-    //     Key: `dist/${id}${filePath}`
-    // }).promise();
+    const contents = await s3.getObject({
+        Bucket: "vercel",
+        Key: `dist/${id}${filePath}`
+    }).promise();
     
-    // const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
-    // res.set("Content-Type", type);
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
 
-    // res.send(contents.Body);
+    res.send(contents.Body);
 })
 
 app.listen(3001);
