bfc221961d3862db5ed4512cc910fa86300482a5
zunxii <zunxii.2210@gmail.com>
2025-08-06 21:12:59 +0530

    request service

---
+++ request-handler-service/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { S3 } from "aws-sdk";
+
+const s3 = new S3({
+    accessKeyId: process.env.ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.SECRET_ACCESS_KEY || "",
+    endpoint: process.env.ENDPOINT  || ""
+})
+
+const app = express();
+
+app.get("/*", async (req, res) => {
+    const host = req.hostname;
+
+    const id = host.split(".")[0];
+    const filePath = req.path;
+
+    const contents = await s3.getObject({
+        Bucket: "vercel",
+        Key: `dist/${id}${filePath}`
+    }).promise();
+    
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
+
+    res.send(contents.Body);
+})
+
+app.listen(3001);
